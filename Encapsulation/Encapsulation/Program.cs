fb600a15ab51aa958187bf6375afda1fe802d374
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-11 10:15:45 +0530

    Add files via upload

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Encap
+{
+    private int id;
+    private string name;
+
+    public void setid(int id)
+    {
+        this.id = id;
+    }
+    public int getid()
+    {
+        return id;
+    }
+    public void setname(String Name)
+    {
+        this.name = Name;
+    }
+    public string getName()
+    {
+        return name;
+    }
+}
+    public class program
+    {
+        public static void Main(String[] args) {
+
+            Encap en = new Encap();
+            en.setid(101);
+            Console.WriteLine(en.getid());
+            en.setname("Somu");
+            Console.WriteLine(en.getName());
+
+        }
+
+    }
---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int n = 15, p = 1;
+        for (int i = n; i >= 1; i--)
+        {
+            p *= i;
+        }
+        Console.WriteLine("Factorial Number OF "+n+" "+p);
+    }
+}
