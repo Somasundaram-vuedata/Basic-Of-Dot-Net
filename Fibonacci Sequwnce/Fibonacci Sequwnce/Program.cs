7cfcd15d60f77226737d322e692745e0de34f32b
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-21 07:05:56 +0530

    Add files via upload

---
+++ Fibonacci Sequwnce/Fibonacci Sequwnce/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program {
+    public static void Main(string[] args) {
+        int a, b, c, n;
+        a = 0; b = 1;
+        Console.Write("Enter The Number Of Terms: ");
+        n = int.Parse(Console.ReadLine());
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        for (int i = 1; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            Console.WriteLine(c);
+        }
+            Console.ReadLine();
+        
+    }
+}
---
+++ SwapNumberBitWiseOperator/SwapNumberBitWiseOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        Console.WriteLine("Before Swap Number A :"+a+" B:"+b);
+        a = a ^ b;
+        b = a ^ b;
+        a= a ^ b;
+        Console.WriteLine("After Swap Number A :" + a + " B:" + b);
+
+    }
+}
