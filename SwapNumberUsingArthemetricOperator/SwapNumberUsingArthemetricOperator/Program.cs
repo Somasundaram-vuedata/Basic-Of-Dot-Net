b320870336e4cc08fbe21eb9e4916ced5492a6c8
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-21 07:07:05 +0530

    Add files via upload

---
+++ SwapNumberUsingArthemetricOperator/SwapNumberUsingArthemetricOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int a = 47;
+        int b = 49;
+        Console.WriteLine("Before Swap Number A :"+a+" B:"+b);
+        a = a + b;//47+49==96
+        b = a - b;//96-49=47
+        a = a - b;//96-47=49
+        Console.WriteLine("After Swap Number A :" + a + " B:" + b);
+
+    }
+}
---
+++ SwapNumberUsingTempVariable/SwapNumberUsingTempVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        int a = 45;
+        int b = 46;
+        Console.WriteLine("Before The Swap Number A:" + a + " B:" + b);
+        // using Variable Temperature key for a;
+        int temp = a; 
+        a = b;
+        b = temp;
+        Console.WriteLine("After The Swap Number A: " + a +" B: "+b);
+
+    }
+}
