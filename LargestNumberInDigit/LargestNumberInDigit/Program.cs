56e09afe63140be1810e2fd8a3e6c0d769aa1b23
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-24 20:19:04 +0530

    Add files via upload

---
+++ LargestNumberInDigit/LargestNumberInDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+public class program
+{
+    public static void Main()
+    {
+        int num = 826345678;
+        int largest = 0;
+        while (num > largest)
+        {
+            int digit = num % 10;
+            if (digit>largest) {
+                largest = digit;
+            }
+            num = num / 10;
+        }
+        Console.WriteLine("Largest:"+largest);
+    }
+}
