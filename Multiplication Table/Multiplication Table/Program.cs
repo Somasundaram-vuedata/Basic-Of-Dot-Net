df444341a0234ca3ca47a59bd78f12a5c33bfbf9
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-24 19:40:59 +0530

    Add files via upload

---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class program{
+public static void Main(String[] args)
+{
+        Console.WriteLine("Enter the Number : ");
+        int num = int.Parse(Console.ReadLine());
+    for(int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine(num + " X " + i + "="+(num * i));
+    }
+}
+}
