ba51c0162359ba5bccc26a8f7241e5b13b34f668
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-26 22:58:20 +0530

    Add files via upload

---
+++ Classes&Object_Program/Classes&Object_Program/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Calcultor
+{ 
+public int Sum(int a,int b)
+    {
+         return a+ b;
+    }
+public int diff(int a, int b)
+    {
+        return a - b;
+    }
+public int multi(int a,int b)
+    {
+        return a * b;
+    }
+}
+public class program { 
+public static void Main(String[]args)
+{
+  Calcultor cal = new Calcultor();
+        Console.WriteLine("Sum Of Two Number "+ cal.Sum(1, 2));
+        Console.WriteLine("Diff Of Two Number "+ cal.diff(1, 2));
+        Console.WriteLine("Multiple Of Two Number "+ cal.multi(1, 2));
+    }
+}
