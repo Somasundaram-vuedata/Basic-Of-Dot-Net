05c32358e1db6ad2541a8e3cc1f4881ac7bb84d2
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-27 23:00:57 +0530

    Add files via upload

---
+++ Inheritance_Program/Inheritance_Program/Program.cs
@@ -0,0 +1,33 @@
+﻿class Animals
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is Eating");
+    }
+}
+class Dog : Animals
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is Barking");
+    }
+}
+class Cat : Animals
+{
+    public void Moew() 
+    {
+        Console.WriteLine("Cat is Moewing");
+    }
+}
+public class Mains
+{
+    public static void Main(String[] args)
+    {
+        Dog d = new Dog();
+        Cat c = new Cat();
+        d.Bark();
+        d.Eat();
+        c.Moew();
+        c.Eat();
+    }
+}
