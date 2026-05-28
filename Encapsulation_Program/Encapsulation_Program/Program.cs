a043756ed41052878d893516e88b4a90c0d39f6e
Somasundaram-vuedata <saisanthosh2015@gmail.com>
2026-05-28 21:32:18 +0530

    Add files via upload

---
+++ Encapsulation_Program/Encapsulation_Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class program
+{
+    private String name;
+    private int Age;
+
+    public void setName(String StudentName)
+    {
+        this.name = StudentName;
+    }
+    public void setAge(int StudentAge)
+    {
+        if (StudentAge > 0)
+        { this.Age = StudentAge; }
+        else { Console.WriteLine("Enter The Correct Age"); }
+    }
+    public string getName() {
+        return name;
+    }
+    public int getAge()
+    {
+        return Age;
+    }
+}
+public class MainClass
+{
+    public static void Main(String[] args) {
+
+        program p = new program();
+        p.setName("Somu");
+        p.setAge(5);
+        Console.WriteLine("Student Name:"+p.getName());
+        Console.WriteLine("Student Age: "+p.getAge());
+    }
+
+}
